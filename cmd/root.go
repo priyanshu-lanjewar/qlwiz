8a119d8039b5b9737aa5f3e8f186ef8acbf3432c
Priyanshu Lanjewar <Priyanshu.Lanjewar@Yahoo.com>
2023-10-13 11:57:54 +0530

    launch link added

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,28 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"os"
 
+	"github.com/priyanshu-lanjewar/qlwiz/pkg/helpers"
+	"github.com/priyanshu-lanjewar/qlwiz/pkg/manager"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "qlwiz",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "qlwiz = quicklink wizard",
+	Long:  `Use this tool to bookmark your quickly accessible link, group them`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		if !helpers.ConfigExists() {
+			manager.Initialize()
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -39,5 +37,3 @@ func Execute() {
 func init() {
 
 }
-
-
